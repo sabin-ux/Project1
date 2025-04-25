880e7f71f1ddc0ed22cd12a34f53bbc0f21e9ba7
sabinux <sabin431234@gmail.com>
2025-04-25 10:38:37 -0700

    add some features

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.patch("/books/:id",(req,res)=>{
         message: "books updated successfully"
     })
 })
+postgresql://postgres.hdpqqugzruwjemcgvnie: SABIN@#123$ @aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres
 app.listen(3000,()=>{
     console.log(("server is running on port 3000"))
 })
