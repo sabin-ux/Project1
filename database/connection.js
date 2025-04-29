6b3be21a92f6916e6d2e01af14b40c302831b790
Sabin <sabin431234@gmail.com>
2025-04-29 21:26:21 +0545

    "commit some file"

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,6 @@
-const { Sequelize, DataTypes } = require("sequelize")
+const { Sequelize, DataTypes } = require("sequelize");
+const bookModel = require("./models/bookModel");
+const userModel = require("./models/userModel");
 
 // Create connection
 const sequelize = new Sequelize("postgresql://postgres.hdpqqugzruwjemcgvnie:Wq7nE2vT4kY9bR5m@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres");
@@ -9,13 +11,20 @@ sequelize.authenticate()
     console.log("Connection was successful!");
   })
   .catch((err) => {
-    console.log("Error occurred:");
+    console.log("Error occurred:" + err);
   });
 
 // Export
-const db = {}
+const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+db.book = bookModel(sequelize, DataTypes);
+db.User = userModel(sequelize, DataTypes);
+
+// Migrate
+sequelize.sync({ alter: false }).then(() => {
+  console.log("Project start vayo hai");
+});
 
 module.exports = db;
